41bc0d626d62ca743ce7f1dbfe74aa84ce728af6
Divya Margaret <divya.margaret96@gmail.com>
2021-07-11 00:25:26 -0700

    react-basics

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Sample from './Sample'
+import SampleClass  from './SaampleClass';
+function App(){
+  return <div> <h1>HELLO WORLD!! </h1>
+   <Sample name="user1" last="last1"/>
+   <Sample  name="user2" last="last2"/>
+
+   <Sample name="user3" last="last3"/>
+   <SampleClass name="classuser" last="classlast" name1="cluser1" last1="cllast1"/>
+   
+
+
+
+  </div>;
+}
+
+export default App;
+ 
---
+++ src/Sample.js
@@ -0,0 +1,7 @@
+
+
+
+function Sample(prop){
+    return <h1>Sample.. {prop.name} {prop.last}</h1>;
+}
+export default Sample;
